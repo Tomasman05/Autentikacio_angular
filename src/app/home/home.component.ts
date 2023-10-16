76ab7ec225c8e36c60e483cf593531b4bf081079
Vito <lolozol80xd@gmail.com>
2023-10-16 11:55:33 +0200

    ongoing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BaseService } from '../base.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +9,16 @@ import { BaseService } from '../base.service';
 })
 export class HomeComponent {
   newMessage:any
-  constructor(private base:BaseService){}
+  messages:any
+  constructor(private base:BaseService){
+    this.base.get().snapshotChanges().pipe(
+      map(ch=>ch.map(c=>({key: c.payload.key, ...c.payload.val()})))
+    ).subscribe((uzenetek)=>this.messages=uzenetek)
+  }
   elkuld(){
-    this.base.createMessage(this.newMessage).then(()=>this.newMessage="")
+    let body:any = {}
+    body.committer = "Én vagyok !"
+    body.message=this.newMessage
+    this.base.createMessage(body).then(()=>this.newMessage="")
   }
 }
--- src/app/message.ts
+++ src/app/message.ts
@@ -1,4 +1,5 @@
 export class Message {
     key?:string
+    committer?:string
     message?:string
 }
