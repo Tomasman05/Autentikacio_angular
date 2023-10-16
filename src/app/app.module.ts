404940176c54efa12bfc8afea79e489052f14be6
Vito <lolozol80xd@gmail.com>
2023-10-16 09:06:07 +0200

    Autentikacio kezdes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from './nav/nav.component';
+import { Environments } from './environments';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignUpComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    AngularFireModule.initializeApp(Environments.firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/environments.ts
@@ -0,0 +1,14 @@
+export const Environments = {
+    production: false,
+    
+    firebaseConfig: {
+        apiKey: "AIzaSyBBwQrjOnMjqYX1BBy2HP8Y9n6y-qpxj9w",
+        authDomain: "authentication-8a6a0.firebaseapp.com",
+        databaseURL: "https://authentication-8a6a0-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "authentication-8a6a0",
+        storageBucket: "authentication-8a6a0.appspot.com",
+        messagingSenderId: "1065382659494",
+        appId: "1:1065382659494:web:4b4fa38c0a36a0869cfb67",
+        measurementId: "G-2GK4326J1W"
+    }
+}
